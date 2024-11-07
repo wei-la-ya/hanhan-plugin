b57a621e198e9ed53fb46485f03d0e80f4fc0073
维拉 <142399519+wei-la-ya@users.noreply.github.com>
2024-11-07 18:44:30 +0800

    undefined的情况为协议端问题，非插件问题，回退代码

--- apps/tou.js
+++ apps/tou.js
@@ -28,14 +28,13 @@ export class example extends plugin {
     } else if (e.message[1].text) {
       name = e.message[1].text.replace('@', '')
     }
-    let username2 = e.sender.nickname
-    let username = e.sender.nickname
+    let name2 = e.sender.nickname
     let url2 = `https://q1.qlogo.cn/g?b=qq&s=0&nk=${at}`
     let url3 = `https://q1.qlogo.cn/g?b=qq&s=0&nk=${e.user_id}`
     let image = segment.image(url2)
     let image2 = segment.image(url3)
-    let cao = `你与${username}发起pk，你长枪一挺，精准一枪洞穿${username}。。。${username}飞出几十米远撞在远处的崖壁上，晕了1分钟在晕的60秒内整整被你透了60次，${username}卒。`
-    let cao2 = `你刚想透${username}的时候，被${username2}猛然一个转身擒拿住了，对你使用疯狂乱透将你反杀，皮燕子都被${username2}橄榄了。之后，${username2}提上枪潇洒离去，你躺在地上足足有1分钟动弹不得，${username2}卒。`
+    let cao = `你与${name}发起pk，你长枪一挺，精准一枪洞穿${name}。。。${name}飞出几十米远撞在远处的崖壁上，晕了1分钟在晕的60秒内整整被你透了60次，${name}卒。`
+    let cao2 = `你刚想透${name}的时候，被${name}猛然一个转身擒拿住了，对你使用疯狂乱透将你反杀，皮燕子都被${name}橄榄了。之后，${name}提上枪潇洒离去，你躺在地上足足有1分钟动弹不得，${name2}卒。`
     if (randomType < gailv) {
       e.reply(cao2)
       await common.sleep(3000)
